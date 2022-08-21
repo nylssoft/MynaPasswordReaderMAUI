d310fcb533a1f8c0cbae6cf11f166ca5f0578233
Niels Stockfleth <nyls@aol.com>
2022-08-21 06:35:19 +0200

    ScrollView added
    - Added scroll view for note item page and profile page
    - use model.InitAsync to initialize model

--- PasswordReader/AppShell.xaml.cs
+++ PasswordReader/AppShell.xaml.cs
@@ -42,16 +42,7 @@ public partial class AppShell : Shell
             try
             {
                 _init = true;
-                _model.Username = App.ContextService.GetUsername();
-                _model.UserPhotoUrl = App.ContextService.GetUserPhotoUrl();
-                _model.Password = "";
-                _model.SecurityCode = "";
-                _model.EncryptionKey = await App.ContextService.GetEncryptionKeyAsync();
-                _model.IsLoggedIn = App.ContextService.IsLoggedIn();
-                _model.Requires2FA = App.ContextService.Requires2FA();
-                _model.HasLoginToken = await App.ContextService.HasLoginTokenAsync();
-                _model.HasPasswordItems = App.ContextService.HasPasswordItems();
-                _model.PasswordItems = null;
+                await _model.InitAsync();
             }
             catch (Exception ex)
             {
